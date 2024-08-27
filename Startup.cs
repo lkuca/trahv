7a8046a5b6fdad7c62d6d5044a6737a61ba62565
opilane <opilane@HP-CZC2349HTQ>
2024-08-27 13:13:33 +0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(trahv.Startup))]
+namespace trahv
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
